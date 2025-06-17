e3093841902ca55501cdc1ad02ef508ad04d8519
amit4184 <adalvi@nvi.co.in>
2025-06-17 12:52:18 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,14 +53,7 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        ECDesigner : {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
+          blog: {
           showReadingTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
